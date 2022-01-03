4c011df241c584c4e6581517a7ded6b5e502e90b
Víctor Ponz <vitorponz@gmail.com>
2022-01-03 12:20:09 +0100

    Versión 2

---
+++ src/Controller/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/page", name="page")
+     */
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="inicio")
+     */
+    public function inicio(): Response
+    {
+        return new Response("Bienvenido a la web de contactos");
+    }
+}
